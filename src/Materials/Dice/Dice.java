f9dbb62921dab8f58c433faa4d54eee603f5cecd
dani <lutzidan@students.zhaw.ch>
2022-11-15 10:18:47 +0100

    created a dice object

--- src/Materials/Dice/Dice.java
+++ src/Materials/Dice/Dice.java
@@ -1,4 +1,27 @@
 package Materials.Dice;
-
 public class Dice {
+    private final int MAX = 6;  // maximum face value
+
+    private int value;  // current value showing on the die
+
+    public Dice() {value = roll();}
+
+    /**
+     * Rolle the dice
+     * @return rolled dice value
+     */
+    private int roll()
+    {
+        value = (int)(Math.random() * MAX) + 1;
+        return value;
+    }
+
+    /**
+     * @return String representation of the dice
+     */
+    public String toString()
+    {
+        String result = Integer.toString(value);
+        return result;
+    }
 }
